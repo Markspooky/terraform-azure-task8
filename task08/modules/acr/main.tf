e1b9fa2fdeb4427b793806574c045efa600a55d5
Mark Nagy <mark_nagy@epam.com>
2025-09-29 17:49:20 +0000

    14asd

--- task08/modules/acr/main.tf
+++ task08/modules/acr/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_container_registry" "this" {
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = var.sku
-  admin_enabled       = false
+  admin_enabled       = true
   tags                = var.tags
 }
 
